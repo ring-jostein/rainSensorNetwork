ee5963abddf6c88e800159a95572b397e4b4c341
ring-jostein <54304070+ring-jostein@users.noreply.github.com>
2021-02-12 13:39:55 +0100

    Update arduinoSender.ino

--- arduinoSender/arduinoSender.ino
+++ arduinoSender/arduinoSender.ino
@@ -11,23 +11,26 @@ void setup() // put your setup code here, to run once
   pinMode(ledPin, OUTPUT);
   pinMode(interruptPinRTC, INPUT_PULLUP);
   Serial.begin(115200);
+  
+  tm.Hour = 23;
+  tm.Minute = 31;
+  tm.Second = 55;
+  tm.Day = 13;
+  tm.Month = 2;
+  tm.Year = 2009 - 1970;    //tmElements_t.Year is the offset from 1970
+  RTC.write(tm);  //set the RTC from the tm structure
+
+  RTC.squareWave(SQWAVE_NONE);
+  
   RTC.setAlarm(ALM2_EVERY_MINUTE, 0, 0, 0, 0);  //setter alarm på RTC til å gå av hvert minutt
   RTC.alarm(ALARM_2);
   RTC.alarmInterrupt(ALARM_2, true);
   attachInterrupt(digitalPinToInterrupt(interruptPinRTC), alarmRTC, FALLING);
-
-  tmElements_t tm;
-  tm.Hour = 23;             //set the tm structure to 23h31m30s on 13Feb2009
-  tm.Minute = 31;
-  tm.Second = 30;
-  tm.Day = 13;
-  tm.Month = 2;
-  tm.Year = 2009 - 1970;    //tmElements_t.Year is the offset from 1970
-  RTC.write(tm);            //set the RTC from the tm structure
 }
 
 void loop() // put your main code here, to run repeatedly
 {
+  digitalWrite(ledPin, state);
   RTC.read(tm);
   int t = RTC.temperature();
   float celcius = t / 4.0;
@@ -39,12 +42,10 @@ void loop() // put your main code here, to run repeatedly
   Serial.print(" Temperatur: ");
   Serial.print(celcius);
   Serial.println(" Celcius");
-  digitalWrite(ledPin, state);
   delay(1000);
 }
 
 void alarmRTC() //funksjon som interrupter ved å gi alarm
 {
   state = !state;
-  RTC.alarm(ALARM_2);
 }
