93494020b64202ca65658a59935bfe360d914f67
ring-jostein <54304070+ring-jostein@users.noreply.github.com>
2021-02-16 14:44:05 +0100

    Update arduinoSender.ino
    Detekterer om den våkner på timer eller nedbørmåler. Lagt til fler kommentarer.

--- arduinoSender/arduinoSender.ino
+++ arduinoSender/arduinoSender.ino
@@ -5,10 +5,11 @@
 const byte interruptPinRTC = 2;
 const byte interruptPinRainGauge = 3;
 tmElements_t tm;
+int t = 0;
 
 void setup() // put your setup code here, to run once
 {
-  
+  digitalWrite(LED_BUILTIN, LOW); //strømsparing
   pinMode(interruptPinRTC, INPUT_PULLUP);
   pinMode(interruptPinRainGauge, INPUT_PULLUP);
   Serial.begin(2000000);
@@ -23,7 +24,8 @@ void setup() // put your setup code here, to run once
   tm.Year = 2021 - 1970;    //tmElements_t.Year is the offset from 1970
   RTC.write(tm);  //set the RTC from the tm structure
   */
-
+  
+  //Blokk som setter alarmer til kjente verdier
   RTC.setAlarm(ALM1_MATCH_DATE, 0, 0, 0, 1);
   RTC.setAlarm(ALM2_MATCH_DATE, 0, 0, 0, 1);
   RTC.alarm(ALARM_1);
@@ -31,9 +33,9 @@ void setup() // put your setup code here, to run once
   RTC.alarmInterrupt(ALARM_1, false);
   RTC.alarmInterrupt(ALARM_2, false);
   RTC.squareWave(SQWAVE_NONE);
-  
+
   RTC.setAlarm(ALM2_EVERY_MINUTE, 0, 0, 0, 0);  //setter alarm på RTC til å gå av hvert minutt
-  RTC.alarmInterrupt(ALARM_2, true);
+  RTC.alarmInterrupt(ALARM_2, true);            //aktiverer alarm slik at INT flagger når alarm går av
 }
 
 void loop() // put your main code here, to run repeatedly
@@ -48,37 +50,43 @@ void sleepMode()
   attachInterrupt(digitalPinToInterrupt(interruptPinRTC), wakeUp1, FALLING);
   attachInterrupt(digitalPinToInterrupt(interruptPinRainGauge), wakeUp2, FALLING);
   set_sleep_mode(SLEEP_MODE_PWR_DOWN);
-  digitalWrite(LED_BUILTIN, LOW);
   sleep_cpu();
+  
   Serial.println("Waking up...");
-  digitalWrite(LED_BUILTIN, HIGH);
-  
   RTC.read(tm);
-  int t = RTC.temperature();
+  t = RTC.temperature();
   float celcius = t / 4.0;
-  Serial.print(tm.Hour, DEC);
-  Serial.print(':');
-  Serial.print(tm.Minute, DEC);
-  Serial.print(':');
-  Serial.print(tm.Second, DEC);
-  Serial.print(" Temperatur: ");
-  Serial.print(celcius);
-  Serial.println(" Celcius");
+
+  if (RTC.checkAlarm(ALARM_2)) //sjekker for om Arduino våknet av alarm
+  {
+    Serial.println("Interrupted sleep from alarm.");
+    Serial.print(tm.Hour, DEC);
+    Serial.print(':');
+    Serial.print(tm.Minute, DEC);
+    Serial.print(':');
+    Serial.print(tm.Second, DEC);
+    Serial.print(" Temperatur: ");
+    Serial.print(celcius);
+    Serial.println(" Celcius");
+    RTC.clearAlarm(ALARM_2);
+  }
   
-  RTC.alarm(ALARM_2);
+  else //Arduino vekket av nedbørsmåling
+  {
+    Serial.println("Interrupted sleep from rain gauge.");
+    Serial.println("Nedbørsmåling");
+  }
 }
 
 
 void wakeUp1() //funksjon som interrupter ved å gi alarm
 {
   detachInterrupt(digitalPinToInterrupt(interruptPinRTC));
-  Serial.println("Interrupted sleep from alarm.");
   sleep_disable();
 }
 
 void wakeUp2()
 {
   detachInterrupt(digitalPinToInterrupt(interruptPinRainGauge));
-  Serial.println("Interrupted sleep from rain gauge.");
   sleep_disable();
 }
