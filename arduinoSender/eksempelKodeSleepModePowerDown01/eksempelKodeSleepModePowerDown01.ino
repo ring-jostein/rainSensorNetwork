87e0dd7695368bd94e6a6afecdb37a4c4679edad
Taicko <78801515+Taicko@users.noreply.github.com>
2021-02-16 12:45:06 +0100

    Update eksempelKodeSleepModePowerDown01.ino

--- arduinoSender/eksempelKodeSleepModePowerDown01/eksempelKodeSleepModePowerDown01.ino
+++ arduinoSender/eksempelKodeSleepModePowerDown01/eksempelKodeSleepModePowerDown01.ino
@@ -5,13 +5,15 @@ kabelen ut fra GND før kortet går i sleep mode igjen, dras ikke kabelen ut fø
 vil ikke interrupten funke lengre
 */
 #include <avr/sleep.h> //this AVR library contains the method that controls the sleep modes
-#define interuptPin 2 //Pin we are going to use to wake up the Arduino
+#define interuptPin2 2 //Pin we are going to use to wake up the Arduino
+#define interuptPin3 3 //Pin we are going to use to wake up the Arduino
 
 void setup() {
   // put your setup code here, to run once:
 Serial.begin(115200); //Start serial communication
 pinMode(LED_BUILTIN, OUTPUT); //We use led on pin 13 to indicate when Arduino is a sleep
-pinMode(interuptPin, INPUT_PULLUP); //Set pin d2 to input using the buildtin pullup resistor
+pinMode(interuptPin2, INPUT_PULLUP); //Set pin d2 to input using the buildtin pullup resistor
+pinMode(interuptPin3, INPUT_PULLUP); //Set pin d2 to input using the buildtin pullup resistor
 digitalWrite(LED_BUILTIN, HIGH); //Turning LED on
 }
 
@@ -25,6 +27,7 @@ void goingToSleep()
 {
   sleep_enable(); //Enable sleep
   attachInterrupt(0, wakeUp, LOW); //Attaching a interrupt to pin d2
+  attachInterrupt(1, wakeUp, LOW); //Attaching a interrupt to pin d2
   set_sleep_mode(SLEEP_MODE_PWR_DOWN); //Setting the sleep mode, in our case full sleep
   digitalWrite(LED_BUILTIN, LOW); //Turning LED off
   Serial.println("Going to sleep..."); //Print message befor going to sleep
@@ -39,4 +42,5 @@ void wakeUp()
   Serial.println("Interrupt Fired"); //Print message to serial monitor
   sleep_disable(); //Disable sleep mode
   detachInterrupt(0); //Remove the intertupt from pin 2
+  detachInterrupt(1); //Remove the intertupt from pin 2
 }
