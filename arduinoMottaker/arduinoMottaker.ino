d13802de670041725423c00db42c2b48fc056fc6
ring-jostein <54304070+ring-jostein@users.noreply.github.com>
2021-04-15 00:16:43 +0200

    Update arduinoMottaker.ino

--- arduinoMottaker/arduinoMottaker.ino
+++ arduinoMottaker/arduinoMottaker.ino
@@ -2,16 +2,15 @@
 #include <WiFiNINA.h>
 #include <SD.h>
 
-#define chipSelect 4  //pinne for kommunikasjon med SD-kort
+#define chipSelect 4  //pinne 4 for kommunikasjon med SD-kort for MKR, 10 for Uno
 #define fileName "datalog.txt"
 #define errorSD "Feil: Kan ikke åpne datalog.txt fra SD-kort"
 #define ssid "abc123"
 #define pass "12345678"
 
 int status = WL_IDLE_STATUS;
-
 WiFiServer server(2323);
-WiFiClient clients[4];
+//WiFiClient clients[4];
 
 void setup() 
 {
@@ -54,6 +53,18 @@ void loop()
 {
   WiFiClient client = server.available();
   
+  while (client.connected())
+  {
+    while (client.available())
+    {
+      byte buffer[160];
+      int count = client.read(buffer, 160);
+      Serial.write(buffer, count);
+      lagreTilSD(buffer, count);
+    }
+  }
+  
+  /*
   if (client)
   {
     //Serial.println(F("Ny klient"));
@@ -71,6 +82,53 @@ void loop()
     client.stop();
     Serial.println("Klient frakoblet");
   }
+  */
+  
+  /*
+  WiFiClient newClient = server.accept();
+  
+  if (newClient) 
+  {
+    for (byte i=0; i < maxClients; i++) 
+    {
+      if (!clients[i]) 
+      {
+        Serial.print(F("Ny klient #"));
+        Serial.println(i);
+        newClient.print(F("Hello, client number: "));
+        newClient.println(i);
+        clients[i] = newClient;
+        break;
+      }
+    }
+  }
+  
+  // Sjekker innkommende data fra klienter
+  for (byte i=0; i < maxClients; i++) 
+  {
+    if (clients[i] && clients[i].available() > 0) 
+    {
+      // les bytes fra klient
+      byte buffer[80];
+      int count = clients[i].read(buffer, 80);
+      Serial.write(buffer, count);  //kun for testing
+      Serial.println();  //kun for testing
+      lagreTilSD(buffer, count);
+      //lesFraSD();
+      
+      //print bekreftelse
+      clients[i].println(F("OK"));
+    }
+  }
+  // Fjerner klienter som kobler av fra array
+  for (byte i=0; i < maxClients; i++) {
+    if (clients[i] && !clients[i].connected()) {
+      Serial.print(F("Kobler fra klient #"));
+      Serial.println(i);
+      clients[i].stop();
+    }
+  }
+  */
 }
 
 void printWiFiStatus() 
