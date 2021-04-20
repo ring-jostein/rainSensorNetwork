7e60de39849b46ee8c6aab3591ea67b210a941a4
ring-jostein <54304070+ring-jostein@users.noreply.github.com>
2021-04-20 11:48:06 +0200

    Kommentert koden bedre, lagt SSID og passord i egen fil

--- arduinoMottaker/arduinoMottaker.ino
+++ arduinoMottaker/arduinoMottaker.ino
@@ -2,20 +2,20 @@
 #include <WiFiNINA.h>
 #include <SD.h>
 #include <Ethernet.h>
+#include "secrets.h"
 
-#define chipSelect 4  //pinne 4 for kommunikasjon med SD-kort for MKR, 10 for Uno
-#define fileName "datalog.txt"
-#define errorSD "Feil: Kan ikke åpne datalog.txt fra SD-kort"
-#define ssid "abc123"
-#define pass "12345678"
+#define chipSelect 4  // pinne 4 for kommunikasjon med SD-kort for MKR, 10 for Uno
+#define fileName "datalog.txt"  // Filnavn for data på SD-kort
+#define errorSD "Feil: Kan ikke åpne datalog.txt fra SD-kort"  // Felles feilmelding flere steder i koden
+
+const char ssid[] = SECRET_SSID;
+const char pass[] = SECRET_PASS;
 
 int status = WL_IDLE_STATUS;
 WiFiServer server(2323);
 EthernetClient ethClient;
 
-// Enter a MAC address and IP address for your controller below.
-// The IP address will be dependent on your local network.
-// gateway and subnet are optional:
+// Sett MAC- og IP-adresse for ethernetkontroller under. Gateway og subnet er valgfri
 byte mac[] = {0xDE, 0xAD, 0xBE, 0xEF, 0xFE, 0xED};
 IPAddress ip(192, 168, 1, 177);
 IPAddress myDns(192, 168, 1, 1);
@@ -30,13 +30,15 @@ void setup()
   Ethernet.begin(mac, ip, myDns, gateway, subnet);
 
   //  Tester kommunikasjon med Ethernet Modul
-  if (Ethernet.hardwareStatus() == EthernetNoHardware) {
+  if (Ethernet.hardwareStatus() == EthernetNoHardware) 
+  {
     Serial.println(F("Feil: Finner ikke Ethernet-modul"));
     while (true);  //Ikke fortsett
   }
   if (Ethernet.linkStatus() == LinkOFF) 
   {
     Serial.println(F("Ethernetkabel er ikke tilkoblet"));
+    while (true);  //Ikke fortsett
   }
   
   // Tester kommunikasjon med WiFi-modul
@@ -74,25 +76,25 @@ void setup()
 
 void loop() 
 {
-  WiFiClient client = server.available();
+  WiFiClient client = server.available();  // Ny klient basert på innkommende data
 
-  int noCharCount = 0;
+  int noCharCount = 0; //Teller runder koden går uten innkommende data
   if (client)
   {
     while (client.connected())
     {
       while (client.available())
       {
-        byte buffer[160];
-        int count = client.read(buffer, 160);
-        Serial.write(buffer, count);
-        //lagreTilSD(buffer, count);
+        byte buffer[160];  // Buffer for data
+        int count = client.read(buffer, 160); // Skriver data til buffer og teller lengden
+        Serial.write(buffer, count); // Echo data ut på Serial
+        lagreTilSD(buffer, count);  // Lagrer data til SD-kort
 
-        noCharCount = 0;
+        noCharCount = 0; // Resetter teller for runder uten innkommende data
       }
-      delay(10);
       noCharCount++;
 
+      // Timeout-funksjon i tilfelle klient ikke kobler fra server
       if (noCharCount > 1000)
       {
         Serial.println();
@@ -115,6 +117,7 @@ void printWiFiStatus()
   Serial.println(ip);
 }
 
+// Funksjon for å lagre data til SD-kort
 void lagreTilSD(byte buffer[160], int count)
 {
   File dataFil = SD.open(fileName, FILE_WRITE);
@@ -127,6 +130,7 @@ void lagreTilSD(byte buffer[160], int count)
   else Serial.println(F(errorSD));
 }
 
+// Testfunksjon for å verifisere at data blir skrevet til SD-kort
 void lesFraSD()
 {
   File dataFil = SD.open(fileName);
---
+++ arduinoMottaker/secrets.h
@@ -0,0 +1,3 @@
+// Definer SSID og passord for aksesspunkt
+#define SECRET_SSID "abc123"
+#define SECRET_PASS "12345678"
